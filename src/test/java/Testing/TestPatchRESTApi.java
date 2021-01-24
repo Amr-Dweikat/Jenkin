1772c508b0373739a2a5d737d08e0d183ab61e13
Amr Dweikat <AmrD@exalt.ps>
2021-01-24 12:06:51 +0200

    first commit

---
+++ src/test/java/Testing/TestPatchRESTApi.java
@@ -0,0 +1,33 @@
+package Testing;
+
+import Classes.Help;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+public class TestPatchRESTApi {
+
+    private final static String POST_URL = "https://jsonplaceholder.typicode.com/posts/";
+    private final static String POST_FIELDS[] = {"id" , "title" };
+    private Help help = new Help();
+
+    @Test
+    public void testPatchMethodForPostGotByID(){
+        String newPostDataFields []= {"5000","first step for automation testing"};
+        JSONObject data = help.createPostData(POST_FIELDS , newPostDataFields);
+        help.validatePatchMethodForPostGotByID(POST_URL , 10 , data);
+    }
+
+    @Test
+    public void testPatchMethodForPostGotByWrongID(){
+        String newPostDataFields []= {"5000","testing"};
+        JSONObject data = help.createPostData(POST_FIELDS , newPostDataFields);
+        help.validatePatchMethodForPostGotByWrongID(POST_URL , 400 , data);
+    }
+
+    @Test
+    public void testPatchMethodForWrongDataPostGotByID(){
+        String newPostDataFields []= {"amr","learning"};
+        JSONObject data = help.createPostData(POST_FIELDS , newPostDataFields);
+        help.validatePatchMethodForWrongDataPostGotByID(POST_URL , 5, data);
+    }
+}
---
+++ src/test/java/Testing/TestPostRESTApi.java
@@ -0,0 +1,27 @@
+package Testing;
+
+import Classes.Help;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+public class TestPostRESTApi {
+
+    private final static String POST_URL = "https://jsonplaceholder.typicode.com/posts";
+    private final static String POST_FIELDS[] = {"userId" , "id" , "title" , "body"};
+    private Help help = new Help();
+
+
+    @Test
+    public void testPostMethodForPosts(){
+        String postDataFields []= {"1000","1000","automation testing","learning"};
+        JSONObject data = help.createPostData(POST_FIELDS , postDataFields);
+        help.validatePostMethodForPosts(POST_URL , data);
+    }
+
+    @Test
+    public void testPostMethodForWrongDataPosts(){
+        String postDataFields []= {"amr","dweikat","automation testing","learning"};
+        JSONObject data = help.createPostData(POST_FIELDS , postDataFields);
+        help.validatePostMethodForWrongDataPosts(POST_URL , data);
+    }
+}
